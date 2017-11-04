a8f20bfad6d8324bc8c461d7c5fa389bc9f0a6b8
Dat Tran <dat.tran@beatly.com>
2017-11-04 14:50:00 +0700

    Update URL

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://goodhabit.fullstuck.com/',
+    assetsPublicPath: 'http://goodhabit.fullstuck.com/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
